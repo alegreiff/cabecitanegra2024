9280864fc33ea3bf7f2491a30e9bcfa2216f77d3
Jaime de Greiff <alegreiff@gmail.com>
2024-10-29 21:17:47 -0500

    RUTA

--- src/stores/proyectos.ts
+++ src/stores/proyectos.ts
@@ -608,7 +608,7 @@ export const personas:Personas[] = [
   <p>Sus historias están envueltas por un halo de melancolía con personajes particulares con mundos internos llenos de matices. Así como disfruta crear historias que navegan entre géneros como el drama, terror psicológico y el misterio, así mismo disfruta crear para público infantil y juvenil. Docente con más de diez años de experiencia.</p>`,
       en: `Colombian director, screenwriter and editor. He studied filmmaking at National University of Colombia. His first feature film, The Crack (2012) was released at the Busan International Film Festival and its sales agent is E One from Canada. Director and scriptwriter of the film Rage (2020). Director of Laika in the Stars (2020) a TV series, and the shortfilm for children Doze (2019).`
     },
-    img: 'alfonso.png'
+    img: '/alfonso.png'
   },
   {
     nombres: "Carolina Mosquera",
@@ -620,7 +620,7 @@ export const personas:Personas[] = [
       es: `Productora colombiana involucrada creativamente en todos los procesos de producción de sus proyectos. Es Realizadora de cine y televisión de la Universidad Nacional de Colombia, promoción 2007. Productora EAVE - Puentes promoción 2015 - 2016. Becaria Torino Film Lab – Idartes 2019. Ganadora de múltiples premios del Fondo para el Desarrollo Cinematográfico y del Programa Nacional de Estímulos del Ministerio de Cultura. Actualmente adelanta estudios en la Maestría en Estética e Historia del arte en la universidad Jorge Tadeo Lozano.`,
       en: `Colombian producer. EAVE – PUENTES promotion 2015/2016. Graduated from Film School of the Universidad Nacional de Colombia. She´s a producer involved creatively in all processes of production for her projects. Producer of the feature film The Crack (2012, 101 min.) premiered at Busan International Film Festival. Producer, scriptwriter and lead actress of Rage (2020, 100 min).`
     },
-    img: 'carolina.jpg'
+    img: '/carolina.jpg'
   },
   
 
